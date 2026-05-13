757d83518f48a7c6595b51c8627c31829972d5cf
Shresth2929 <Shresthveer0408@gmail.com>
2026-05-13 14:19:38 +0530

    fix cors for vercel frontend

--- server/index.js
+++ server/index.js
@@ -19,7 +19,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://tunify-ie.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
